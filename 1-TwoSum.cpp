2243dd03834db4a343fbf0d3c017919616df8353
michalmaksymow <borysbrat@gmail.com>
2021-04-05 11:27:42 +0200

    Initial commit

---
+++ 1-TwoSum.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+    Solution 1 - Brute force
+    
+    Time complexity: O(n^2)
+    Space complexity: O(1)
+*/
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        vector<int> out;
+        for (size_t i = 0; i < nums.size(); ++i)
+        {
+            for (size_t j = 0; j < nums.size(); ++j)
+            {
+                if (i == j)
+                    continue;
+                if (nums[i] + nums[j] == target)
+                {
+                    out.push_back(i);
+                    out.push_back(j);
+                    return out;
+                }
+            }
+        }
+        return out;
+    }
+};
+
+
+/*
+    Solution 2 - Map
+
+    Time complexity: O(n)
+    Space complexity: O(n)
+*/
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        map<int, int> num; // <number, index>
+        vector<int> out;
+        for (size_t i = 0; i < nums.size(); ++i)
+        {
+            int needed = target - nums[i];
+            if (num.find(needed) != num.end())
+            {
+                out.push_back(i);
+                out.push_back(num[needed]);
+                return out;
+            }
+            num.insert(make_pair(nums[i], i));
+        }
+        return out;
+    }
+};
