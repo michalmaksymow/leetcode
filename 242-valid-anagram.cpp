5046fe5938e8de584ddc8f2f0f626f7fe494b884
michalmaksymow <borysbrat@gmail.com>
2022-06-16 21:21:54 +0200

    Create 242

---
+++ 242-valid-anagram.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#include "utils.hpp"
+
+using namespace std;
+
+// Solution 1 - HashMap
+// Time complexity: O(s)
+// Space complexity: O(s+t)
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) 
+            return false;
+
+        std::map<char, int> sm, tm;
+        for (size_t i = 0; i < s.length(); ++i) {
+            sm[s[i]]++;
+            tm[t[i]]++;
+        }
+
+        for (auto const& key : sm) {
+            if (tm.find(key.first) == tm.end() || tm[key.first] != key.second)
+                return false;
+        }
+        return true;
+    }
+};
+
+int main() {
+    Solution s;
+    string s1 = "rat"; 
+    string s2 = "car";
+
+    cout << boolToString(s.isAnagram(s1, s2)) << endl;
+}
