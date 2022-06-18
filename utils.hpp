169dd41329d155e7ee724fe7575103ee33668e68
michalmaksymow <borysbrat@gmail.com>
2022-06-18 10:25:18 +0200

    Update utils

--- utils.hpp
+++ utils.hpp
@@ -1,23 +1,22 @@
 #ifndef UTILS_HPP
 #define UTILS_HPP
 
+#include <sstream>
 #include <string>
 #include <vector>
-#include <sstream>
 
-template<typename T>
-std::string vectorToString(const std::vector<T>& vec) {
-    std::stringstream ss;
-    ss << '[';
-    for (T i: vec)
-        ss << i << ' ';
-    ss << ']';
+template <typename T> std::string vectorToString(const std::vector<T> &vec) {
+  std::stringstream ss;
+  ss << '[';
+  for (T i : vec)
+    ss << i << ' ';
+  ss << ']';
 
-    return ss.str();
+  return ss.str();
 }
 
-std::string boolToString(const bool& boolean) {
-    return boolean ? "true" : "false";
+std::string boolToString(const bool &boolean) {
+  return boolean ? "true" : "false";
 }
 
 #endif // UTILS_HPP
